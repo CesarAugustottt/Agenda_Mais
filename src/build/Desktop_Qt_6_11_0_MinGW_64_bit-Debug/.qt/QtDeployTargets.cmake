c88b61ed3be7134d65b5bdbab75275a0068ed6f3
Raagranatp <Raagranato@gmail.com>
2026-07-06 19:19:15 -0300

    Versao nao quebrada do qt

---
+++ src/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Agenda_FILE C:/comp/codigo/Eng/Agenda_Mais/src/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/Agenda.exe)
+set(__QT_DEPLOY_TARGET_Agenda_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Agenda_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
---
+++ src/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/Agenda.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Agenda.
+# This may be replaced when dependencies are built.
