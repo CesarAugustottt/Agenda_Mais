6e94a74399c8bc855514b49c20d1c14f889ed1d8
Cesar Augusto <cesar.toto@aluno.ufop.edu.br>
2026-07-08 14:43:34 -0300

    Fix: Manutenção no arquivo TelaDataHora.cpp.

--- src/TelaDataHora.cpp
+++ src/TelaDataHora.cpp
@@ -1,5 +1,6 @@
 #include "TelaDataHora.h"
-#include "AppointmentImpl.h" // Inclui a implementação do Handle-Body
+#include ".AppointmentImpl.h" 
+#include "UserImpl.h"
 #include "TelaEspecialidade.h"
 #include <QMessageBox>
 
@@ -56,7 +57,7 @@ void TelaDataHora::processarAgendamento(const std::string& horario, User* patien
         nomeMedico = "Dra. Fulana (Dermatologista)";
     }
 
-    User* doctor= new UserHandle(nomeMedico);
+    User* doctor = new UserHandle(nomeMedico, "000.000.000-00", "doctor@agendamais.com", "123456", UserType::DOCTOR);
 
     // Criando o objeto com a assinatura correta de 6 parâmetros
     Appointment* novoAgendamento = new AppointmentHandle(
@@ -71,26 +72,30 @@ void TelaDataHora::processarAgendamento(const std::string& horario, User* patien
 }
 
 void TelaDataHora::on_btnHora1330_clicked() { 
-    User* usuarioFicticio = new UserHandle("Paciente");
+    // 👈 CORRIGIDO: Passando os 5 parâmetros exigidos pelo seu UserHandle
+    User* usuarioFicticio = new UserHandle("Paciente", "111.111.111-11", "paciente@teste.com", "123456", UserType::PATIENT);
     
     processarAgendamento("13:30", usuarioFicticio); 
+    delete usuarioFicticio; // Deleta após o processamento para não vazar memória
 }
 
 void TelaDataHora::on_btnHora1400_clicked() { 
-    User* usuarioFicticio = new UserHandle("Paciente");
+    User* usuarioFicticio = new UserHandle("Paciente", "111.111.111-11", "paciente@teste.com", "123456", UserType::PATIENT);
     processarAgendamento("14:00", usuarioFicticio); 
+    delete usuarioFicticio;
 }
 
 void TelaDataHora::on_btnHora1430_clicked() { 
-    User* usuarioFicticio = new UserHandle("Paciente");
+    User* usuarioFicticio = new UserHandle("Paciente", "111.111.111-11", "paciente@teste.com", "123456", UserType::PATIENT);
     processarAgendamento("14:30", usuarioFicticio); 
+    delete usuarioFicticio;
 }
 
 void TelaDataHora::on_btnHora1500_clicked() { 
-    User* usuarioFicticio = new UserHandle("Paciente");
+    User* usuarioFicticio = new UserHandle("Paciente", "111.111.111-11", "paciente@teste.com", "123456", UserType::PATIENT);
     processarAgendamento("15:00", usuarioFicticio); 
+    delete usuarioFicticio;
 }
-
 //se apertar em voltar, abre a tela anterior
 void TelaDataHora::on_btnVoltar_clicked() {
     TelaEspecialidade *tela = new TelaEspecialidade(nullptr);
